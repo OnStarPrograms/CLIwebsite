01d950f58637b6bc46a2213f914ada4fedd21cb3
OnStarPrograms <124287152+OnStarPrograms@users.noreply.github.com>
2023-09-26 08:40:28 -0400

    Update javascript.js
    Turn this back if it doesn't work on web

--- javascript.js
+++ javascript.js
@@ -49,7 +49,7 @@ document.addEventListener('keydown', function(event) {
             Blinker.style.left = 100 + "px";
         }
         else{
-            Blinker.style.left = 75 + "px";
+            Blinker.style.left = 25 + "px";
         }
         var content2 = document.createTextNode(theCLI.textContent);
         try {
